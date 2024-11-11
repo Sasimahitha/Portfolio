e01ca3cd0da8bf7a12612996fd5fad51142e0f4e
Sasimahitha <105411649+Sasimahitha@users.noreply.github.com>
2024-11-11 17:39:30 +0530

    Add files via upload
    It's an Online Portfolio, created with html, css and basic Javascript.

---
+++ main.js
@@ -0,0 +1,51 @@
+// var typed = new typed(".text",{
+//     String:["Student","Web Developer"],
+//     typeSpeed:100,
+//     backSpeed :100,
+//     backDelay:1000,
+//     loop:true
+// })
+var options = {
+    strings: ["Student","Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+};
+var typed = new Typed(".text", options);
+
+
+
+
+{/* <section>
+        <div id="portfolio" id="project">
+            <div class="main-text" id="project">
+                <h2>My <span>Internships</span></h2>
+                <div class="portfolio-content">
+                    <div class="row">
+                        <img src = "#">
+                        <div class="layer">
+                            <h5>Web Developement</h5>
+                            <p>I completed a front-end web development internship at Codsoft, where I honed my skills in HTML, CSS, JavaScript, and modern frameworks such as React. During this internship, I collaborated with a dynamic team to design and implement responsive, user-friendly web interfaces. I successfully contributed to several projects, enhancing website performance and user experience. This hands-on experience has equipped me with a strong foundation in front-end technologies and a keen eye for detail, preparing me for future challenges in web development.</p>
+                                <a href="#"><i class='bx bx-link-external' style="color: aliceblue";></i></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <img src = "#">
+                        <div class="layer">
+                            <h5>Python</h5>
+                            <p>I completed a front-end web development internship at Codsoft, where I honed my skills in HTML, CSS, JavaScript, and modern frameworks such as React. During this internship, I collaborated with a dynamic team to design and implement responsive, user-friendly web interfaces. I successfully contributed to several projects, enhancing website performance and user experience. This hands-on experience has equipped me with a strong foundation in front-end technologies and a keen eye for detail, preparing me for future challenges in web development.</p>
+                                <a href="#"><i class='bx bx-link-external' style="color: aliceblue";></i></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <img src = "#">
+                        <div class="layer">
+                            <h5>Full Stack Web Development</h5>
+                            <p>I completed a front-end web development internship at Codsoft, where I honed my skills in HTML, CSS, JavaScript, and modern frameworks such as React. During this internship, I collaborated with a dynamic team to design and implement responsive, user-friendly web interfaces. I successfully contributed to several projects, enhancing website performance and user experience. This hands-on experience has equipped me with a strong foundation in front-end technologies and a keen eye for detail, preparing me for future challenges in web development.</p>
+                                <a href="#"><i class='bx bx-link-external' style="color: aliceblue";></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> */}
